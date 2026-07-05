8f20b0fabb33e672c4fb663b0b11be7e3f925666
Mehar Abdur Rahim <232846019+Abdurahim-2911@users.noreply.github.com>
2026-07-05 18:43:17 +0400

    Deleting Redundancies

--- Project/Legal_Aid_Agent/schemas/__init__.py
+++
@@ -1,9 +0,0 @@
-# Re-export all schema models for convenient imports.
-
-from .case_object import (
-    IntakeSafetyOutput,
-    LegalAnalysisOutput,
-    TranslationOutput,
-    NGOContact,
-    CaseObject,
-)
--- Project/Legal_Aid_Agent/tools/__init__.py
+++
@@ -1,2 +0,0 @@
-# Makes tools a Python package.
-# All tool functions (vector_search, ngo_lookup, pdf_generator) are imported from here.
